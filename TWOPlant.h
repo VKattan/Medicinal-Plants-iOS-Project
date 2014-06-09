5074a557331c2fdb9544fe7fcd9373a89b14aad5
Vkattan <v.kattan@doctors.org.uk>
2014-06-09 23:08:56 +0400

    added a plant class

---
+++ TWOPlant.h
@@ -0,0 +1,13 @@
+//
+//  TWOPlant.h
+//  MedicinalPlants
+//
+//  Created by Taleiven Kattan on 6/9/14.
+//  Copyright (c) 2014 Vishnoo Kattan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TWOPlant : NSObject
+
+@end
---
+++ TWOPlant.m
@@ -0,0 +1,13 @@
+//
+//  TWOPlant.m
+//  MedicinalPlants
+//
+//  Created by Taleiven Kattan on 6/9/14.
+//  Copyright (c) 2014 Vishnoo Kattan. All rights reserved.
+//
+
+#import "TWOPlant.h"
+
+@implementation TWOPlant
+
+@end
